d62a05a830afe0d5fd142894e5cdd0cede838d8d
Answed <schradearwed@gmail.com>
2023-07-15 15:19:56 +0200

    Added Simple calculator

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void StartCalculator(){
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning){
+            System.out.println("Please select the operator: Addition, Subtraction, Multiplication, Division, Exit");
+            String operator = scanner.nextLine();
+            System.out.println("Please enter ur first input");
+            int inp1 = Integer.parseInt(scanner.nextLine());
+            System.out.println("Please enter ur second input");
+            int inp2 = Integer.parseInt(scanner.nextLine())
+            switch (operator) {
+                case "Addition" -> System.out.println(inp1 + inp2);
+                case "Subtraction" -> System.out.println(inp1 - inp2);
+                case "Multiplication" -> System.out.println(inp1 * inp2);
+                case "Division" -> System.out.println(inp1 / inp2);
+                case "Exit" -> isRunning = false;
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++){
-            System.out.println(i);
-        }
+        Calculator.StartCalculator();
     }
 }
